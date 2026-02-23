894d402ae2e3ba12c67602cf20d17b5a2f3a8282
koh-robotics <ovoyage0526@gmail.com>
2026-02-23 21:06:04 +0900

    とりあえず仮完成

--- practice-ex13.cpp
+++ practice-ex13.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
-  int N;
+  int N,a,b,ave;
   cin >> N;
+  vector<int> vec(N);
+  //make average
+  for (int i = 0; i < N; i++) {
+  cin >> vec.at(i);
+  }
+  for (int i = 0; i < N; i++) {
+    a+= vec.at(i); 
+  }
+  ave = a/N;
+  cout << ave << endl;
+
+  for (int i = 0; i < N; i++) {
+    b = ave - vec.at(i);
+    if (b >= 0) {
+      b = b * -1;
+    }
+    cout << b << endl;
+
+  }
+
 }
