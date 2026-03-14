59a27b43a8aad234eda4d6aaa17419380182897b
koh-robotics <ovoyage0526@gmail.com>
2026-03-14 23:16:56 +0900

    not complete

--- ABC/abc449_c.cpp
+++ ABC/abc449_c.cpp
@@ -3,16 +3,20 @@ using namespace std;
  
 int main() {
     string s;
-    int l,r,n,count = 0;
+    int l,r,n,count = 0,d;
+    vector <int> a(26);
     cin >> n >> l >> r;
     cin >> s;
+    vector<vector<int>> b(n, vector<int>(26,0));
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (s.at(i) == s.at(j) && l <= j - i && j - i <= r ){
-                count ++;
-            }
-
+        a.at(s.at(i));
+        b.at(i).at(s.at(i)) = 1;
         }
+    for (int i = 0; i< 26; i++){
+        a.at(i);
     }
-    cout  << count << endl;
+    
+    
+    
+    cout << count << endl;
 }
